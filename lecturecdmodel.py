50de5cecab9e34e63dbfb62208a33217bfbca5fa
J.P.P.Chathurika <119437644+p-chathurika@users.noreply.github.com>
2023-12-17 17:31:55 +0530

    Add files via upload

---
+++ lecturecdmodel.py
@@ -0,0 +1,7 @@
+class LectureCD:
+    def __init__(self, cd_number, title, subject, rental_price, copies):
+        self.cd_number = cd_number
+        self.title = title
+        self.subject = subject
+        self.rental_price = rental_price
+        self.copies = copies
