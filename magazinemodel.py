05085c623b745d62d7d60828cd05a72e663a71c6
J.P.P.Chathurika <119437644+p-chathurika@users.noreply.github.com>
2023-12-17 17:33:16 +0530

    Add files via upload

---
+++ magazinemodel.py
@@ -0,0 +1,8 @@
+class Magazine:
+    def __init__(self, mag_number, title, print_type, subject, rental_price, copies):
+        self.mag_number = mag_number
+        self.title = title
+        self.print_type = print_type
+        self.subject = subject
+        self.rental_price = rental_price
+        self.copies = copies
