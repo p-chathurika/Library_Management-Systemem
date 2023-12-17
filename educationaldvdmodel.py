2be4391ef29265efeb76fe390644d4757c9eb2a3
J.P.P.Chathurika <119437644+p-chathurika@users.noreply.github.com>
2023-12-17 17:31:01 +0530

    Add files via upload

---
+++ educationaldvdmodel.py
@@ -0,0 +1,7 @@
+class EducationalDVD:
+    def __init__(self, dvd_number, title, subject, rental_price, copies):
+        self.dvd_number = dvd_number
+        self.title = title
+        self.subject = subject
+        self.rental_price = rental_price
+        self.copies = copies
